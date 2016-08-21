54b6fc169b4ea2e4d13f02c0d2b8a3ed5db9b24c
Samuel Nelson <valczir.darkvein@gmail.com>
2016-08-21 10:47:45 -0600

    Add test for FuncDecl, categorize tests

--- syntax/decl_test.go
+++ syntax/decl_test.go
@@ -12,7 +12,17 @@ import (
 	"github.com/nelsam/vidar/syntax"
 )
 
-func TestLayers_GenDecl_NoParen(t *testing.T) {
+func TestLayers(t *testing.T) {
+	t.Run("ast.GenDecl", GenDecl)
+	t.Run("ast.FuncDecl", FuncDecl)
+}
+
+func GenDecl(t *testing.T) {
+	t.Run("NoParens", NoParen)
+	t.Run("Parens", Paren)
+}
+
+func NoParen(t *testing.T) {
 	expect := expect.New(t)
 
 	ast := `
@@ -50,4 +60,95 @@ var Foo string
 	expectedStart = strings.Index(ast, "string")
 	expect(start).To.Equal(expectedStart)
 	expect(end).To.Equal(expectedStart + len("string"))
+}
+
+func Paren(t *testing.T) {
+	expect := expect.New(t)
+
+	ast := `
+package foo
+
+var (
+	Foo string
+	Bar int
+)
+`
+	s := syntax.New(syntax.DefaultTheme)
+	err := s.Parse(ast)
+	expect(err).To.Be.Nil()
+
+	layers := s.Layers()
+	expect(layers).To.Have.Len(3)
+
+	keywords := layers[syntax.DefaultTheme.Colors.Keyword]
+	expect(keywords.Spans()).To.Have.Len(2)
+
+	// var keyword
+	start, end := keywords.Spans()[1].Range()
+	expectedStart := strings.Index(ast, "var")
+	expect(start).To.Equal(expectedStart)
+	expect(end).To.Equal(expectedStart + len("var"))
+
+	typs := layers[syntax.DefaultTheme.Colors.Type]
+	expect(typs.Spans()).To.Have.Len(2)
+	start, end = typs.Spans()[0].Range()
+	expectedStart = strings.Index(ast, "string")
+	expect(start).To.Equal(expectedStart)
+	expect(end).To.Equal(expectedStart + len("string"))
+	start, end = typs.Spans()[1].Range()
+	expectedStart = strings.Index(ast, "int")
+	expect(start).To.Equal(expectedStart)
+	expect(end).To.Equal(expectedStart + len("int"))
+}
+
+func FuncDecl(t *testing.T) {
+	expect := expect.New(t)
+
+	ast := `
+package foo
+
+func Foo(bar string) int {
+	return 0
+}
+`
+	s := syntax.New(syntax.DefaultTheme)
+	err := s.Parse(ast)
+	expect(err).To.Be.Nil()
+
+	layers := s.Layers()
+	expect(layers).To.Have.Len(5)
+	// TODO: test paren colors from rainbow parens
+
+	keywords := layers[syntax.DefaultTheme.Colors.Keyword]
+	expect(keywords.Spans()).To.Have.Len(3)
+
+	// func keyword
+	start, end := keywords.Spans()[1].Range()
+	expectedStart := strings.Index(ast, "func")
+	expect(start).To.Equal(expectedStart)
+	expect(end).To.Equal(expectedStart + len("func"))
+
+	// return keyword
+	start, end = keywords.Spans()[2].Range()
+	expectedStart = strings.Index(ast, "return")
+	expect(start).To.Equal(expectedStart)
+	expect(end).To.Equal(expectedStart + len("return"))
+
+	typs := layers[syntax.DefaultTheme.Colors.Type]
+	expect(typs.Spans()).To.Have.Len(2)
+	start, end = typs.Spans()[0].Range()
+	expectedStart = strings.Index(ast, "string")
+	expect(start).To.Equal(expectedStart)
+	expect(end).To.Equal(expectedStart + len("string"))
+	start, end = typs.Spans()[1].Range()
+	expectedStart = strings.Index(ast, "int")
+	expect(start).To.Equal(expectedStart)
+	expect(end).To.Equal(expectedStart + len("int"))
+
+	ints := layers[syntax.DefaultTheme.Colors.Num]
+	expect(ints.Spans()).To.Have.Len(1)
+	start, end = ints.Spans()[0].Range()
+	expectedStart = strings.Index(ast, "0")
+	expect(start).To.Equal(expectedStart)
+	expect(end).To.Equal(expectedStart + 1)
 }
